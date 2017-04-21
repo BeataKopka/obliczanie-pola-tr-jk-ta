94c394cf31328e8127398b8ecf823ec7c42533c2
Beata Kopka <kopka.beata@gmail.com>
2017-04-21 21:10:41 +0200

    zadanie obliczanie pola trójkąta

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// obliczamy pole trójkąta
+var a,
+	h,
+	triangleArea;
+var a = 2;
+var h = 3;
+var triangleArea = 3;
+var a = prompt("podaj a");
+var h = prompt("podaj h");
+var triangleArea = alert(a*h/2);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
