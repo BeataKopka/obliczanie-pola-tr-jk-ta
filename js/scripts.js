209b5debeecb840af4ed5541d4e2fc95d6103cdc
Beata Kopka <kopka.beata@gmail.com>
2017-04-24 22:51:38 +0200

    po uwagach1

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,18 @@
 var a,
 	h,
 	triangleArea;
-var a = 2;
-var h = 3;
-var triangleArea = 3;
-var a = prompt("podaj a");
-var h = prompt("podaj h");
-var triangleArea = alert(a*h/2);
+	a = 2;
+	h = 3;
+	triangleArea = 3;
+	a = prompt("podaj a");
+	h = prompt("podaj h");
+	triangleArea = alert(a*h/2);
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
 
 if(isNaN (a)) {
 	alert("a nie jest liczbą!");
 }
-else {
-
-}
 
 if(isNaN (h)) {
 	alert("h nie jest liczbą!");
-}
-else {
-
 }
